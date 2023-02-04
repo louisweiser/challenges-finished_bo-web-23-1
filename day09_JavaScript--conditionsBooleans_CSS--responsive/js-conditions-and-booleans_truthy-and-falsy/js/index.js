11845e40923f2fa2f3683a79193d077b3bf39d94
Louis Weiser <louis.weiser98@gmail.com>
2023-02-04 18:42:41 +0100

    add/day09

---
+++ day09_JavaScript--conditionsBooleans_CSS--responsive/js-conditions-and-booleans_truthy-and-falsy/js/index.js
@@ -0,0 +1,19 @@
+const truthyOrFalsy = Boolean("5 < 1");
+console.log("The expression is " + truthyOrFalsy);
+
+/*
+
+1   true
+2   false
+3   true
+4   true
+5   false
+6   false
+7   true
+8   true
+9   true
+10  true
+11  true
+12  true
+
+*/
