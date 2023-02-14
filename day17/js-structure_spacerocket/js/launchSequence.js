14ecf6c93fc2b441de14776716cb3eb1d0b3cd03
Louis Weiser <louis.weiser98@gmail.com>
2023-02-14 16:16:37 +0100

    add day17 task2

--- day17/js-structure_spacerocket/js/launchSequence.js
+++ day17/js-structure_spacerocket/js/launchSequence.js
@@ -1,1 +1,15 @@
 // Implement the launch sequence function here and export it as the default export.
+
+import { countdown } from "./core/countdown";
+import { fuel } from "./core/fuel";
+import { liftoff } from "./core/liftoff";
+import { loadPayload } from "./core/load";
+
+export default function launchSequenceFunction() {
+  loadPayload();
+  fuel();
+  for (let i = 5; i > 0; i--) {
+    countdown();
+  }
+  liftoff;
+}
