214b69665bc2b699bfb47cffa005f4d22b266f59
Louis Weiser <louis.weiser98@gmail.com>
2023-02-23 12:59:09 +0100

    all-done

--- day24/react-nesting_buttons-and-children/src/App.js
+++ day24/react-nesting_buttons-and-children/src/App.js
@@ -4,18 +4,18 @@ import "./styles.css";
 export default function App() {
   return (
     <main>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
+      <Button>Nice</Button>
+      <Button>to</Button>
+      <Button>meet</Button>
+      <Button>you!</Button>
     </main>
   );
 }
 
-function Button() {
+function Button({ children }) {
   return (
     <button className="button" type="button">
-      Click me!
+      {children}
     </button>
   );
 }
--- day24/react-nesting_fragments/src/App.js
+++ day24/react-nesting_fragments/src/App.js
@@ -4,10 +4,10 @@ import Box from "./components/Box";
 
 export default function App() {
   return (
-    <main className="flex-container">
+    <>
       <Box color="#007bff" />
       <Box color="#fc3" />
       <Box color="#ff3333" />
-    </main>
+    </>
   );
 }
