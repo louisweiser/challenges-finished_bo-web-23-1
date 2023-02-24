ffb0f06380ac30a9ff2b7450ad5b0f8720b6ab60
Louis Weiser <louis.weiser98@gmail.com>
2023-02-24 14:48:15 +0100

    all-done

--- day25/react-state_emoji-checker/src/App.js
+++ day25/react-state_emoji-checker/src/App.js
@@ -1,56 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles.css";
-
 export default function App() {
-  let code = "?";
-
+  const [pufferfish, setPufferfish] = useState("🐡");
+  const [whale, setWhale] = useState("🐋");
+  const [clownfish, setClownfish] = useState("🐠");
   const validCode = "🐡🐠🐋";
-
+  const handlePufferfishClick = () => {
+    if (pufferfish === "🐡") {
+      setPufferfish("🐠");
+    } else if (pufferfish === "🐠") {
+      setPufferfish("🐋");
+    } else {
+      setPufferfish("🐡");
+    }
+  };
+  const handleWhaleClick = () => {
+    if (whale === "🐡") {
+      setWhale("🐠");
+    } else if (whale === "🐠") {
+      setWhale("🐋");
+    } else {
+      setWhale("🐡");
+    }
+  };
+  const handleClownfishClick = () => {
+    if (clownfish === "🐡") {
+      setClownfish("🐠");
+    } else if (clownfish === "🐠") {
+      setClownfish("🐋");
+    } else {
+      setClownfish("🐡");
+    }
+  };
+  const handleReset = () => {
+    setPufferfish("🐡");
+    setWhale("🐋");
+    setClownfish("🐠");
+  };
   return (
     <div className="container">
       <div className="button-container">
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={handlePufferfishClick}>
           <span role="img" aria-label="Pufferfish">
-            🐡
+            {pufferfish}
           </span>
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={handleWhaleClick}>
           <span role="img" aria-label="Whale">
-            🐋
+            {whale}
           </span>
         </button>
-        <button
-          type="button"
-          onClick={() => {
-            console.log("Update Code!");
-          }}
-        >
+        <button type="button" onClick={handleClownfishClick}>
           <span role="img" aria-label="Clownfish">
-            🐠
+            {clownfish}
           </span>
-        </button>{" "}
+        </button>
       </div>
-
-      <button
-        type="button"
-        onClick={() => {
-          console.log("Reset Code!");
-        }}
-      >
+      <button type="button" onClick={handleReset}>
         Reset
       </button>
-
-      {code === validCode && <p>Valid code!</p>}
+      {pufferfish + whale + clownfish === validCode && <p>Valid code!</p>}
     </div>
   );
 }
