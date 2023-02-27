bec6dc94305bdbf497bf1cac7a20bcd6dc97ca5f
Louis Weiser <louis.weiser98@gmail.com>
2023-02-27 17:02:43 +0100

    onprogress

--- day26/react-state-3_tags/src/App.js
+++ day26/react-state-3_tags/src/App.js
@@ -2,13 +2,18 @@ import { useState } from "react";
 import "./styles.css";
 import Form from "./components/Form/index.js";
 import List from "./components/List/index.js";
+import { uid } from "uid";
 
 export default function App() {
   const [tags, setTags] = useState(["JavaScript", "React", "CSS", "HTML"]);
 
+  function handleAddTag(newTag) {
+    setTags([...tags, { ...newTag, id: uid() }]);
+  }
+
   return (
     <main className="app">
-      <Form />
+      <Form onAddTag={handleAddTag} />
       <List tags={tags} />
     </main>
   );
