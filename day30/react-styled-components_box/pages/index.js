065be9ab58eeea95d0a050107280f3570d6863b3
Louis Weiser <louis.weiser98@gmail.com>
2023-03-06 11:00:15 +0100

    add day31 task1

--- day30/react-styled-components_box/pages/index.js
+++ day30/react-styled-components_box/pages/index.js
@@ -1,10 +1,36 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import styled from "styled-components";
 
 export default function HomePage() {
   return (
     <div>
       <BoxWithClassName />
       <BoxWithClassName isBlack />
+      <BoxWithStyledComponents variant="black" />
     </div>
   );
 }
+
+const BoxWithStyledComponents = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: ${({ variant }) =>
+    variant === "black" ? "black" : "green;"};
+  }};
+  margin: 2rem;
+  &:hover {
+    background-color: red;
+  }
+`;
+
+/* const StyledBox = styled.div`
+  width: 100px;
+  height: 100px;
+  background-color: "green";
+  margin: 2rem;
+  &:hover {
+    background-color: ${  ({isBlack})  =>  } ;
+  }
+`;
+
+function BoxWithStyledComponents({ isBlack }) {} */
